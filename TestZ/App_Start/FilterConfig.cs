0771e1f71336b258411dabd3eab6a9b041e289de
kdinh9 <merikakun@gmail.com>
2016-09-12 03:25:39 +0700

    1

---
+++ TestZ/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestZ
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
